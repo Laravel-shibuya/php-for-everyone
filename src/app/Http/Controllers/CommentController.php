5ec0ef0b6eb524e4fbaeb00a65fee0aede366df3
y_ahiru <yahiru1121@gmail.com>
2019-09-07 23:12:12 +0900

    コメント作成処理を実装

--- src/app/Http/Controllers/CommentController.php
+++ src/app/Http/Controllers/CommentController.php
@@ -6,8 +6,18 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function store()
+    public function store(Request $request, int $postId)
     {
-        //
+        if (!\App\Eloquent\Post::where('id', $postId)->exists()) abort(404);
+
+        $this->validate($request, ['body' => 'required']);
+
+        $comment = new \App\Eloquent\Comment;
+        $comment->body = $request->input('body');
+        $comment->post_id = $postId;
+        $comment->user_id = \Auth::id();
+        $comment->save();
+
+        return redirect(route('posts.show', $postId));
     }
 }
